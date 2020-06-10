358e27db8195cf9b885916b7f113f6a8e371303e
Rigo <skinny229@gmal.com>
2020-06-10 03:38:53 -0400

    Spring back end initial set up

---
+++ backend/src/main/java/com/echovrprotocol/backend/AstreaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.echovrprotocol.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstreaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstreaProjectApplication.class, args);
+	}
+
+}
